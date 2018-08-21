5123e093bd30a5ca8016d0c8bb082c2fd1afcae3
sasa-us <sasa_luu@hotmail.com>
2018-08-21 00:10:09 -0700

    login done

--- assets/js/login-out.js
+++ assets/js/login-out.js
@@ -11,8 +11,8 @@ function init() {
 //     $("#logoutButton").on('click', logoutUser);
 // }
 function loginUser() {
-    var email = $('#email').val();
-    var password = $('#password').val();
+    var email = $('#loginEmail').val();
+    var password = $('#loginPassword').val();
     logindata = {
         email: email,
         password: password,
@@ -24,14 +24,37 @@ function loginUser() {
         dataType: 'json',
         method: 'post',
         success: function(response) {
-            console.log( 'response is ', response);
+            console.log( 'LOGIN response is ', response);
+            //success: true, errors: Array(0), user: {…query selected column namne}
+            if(response.success) {
+                $('#myModal').modal('hide');
+                showWelcome(response.user);
+            }
+                // else {
+            //     showRegiterInfo(response.error);
+            //     activeRegister();
+            // }
         },
         error: function(response) {
+            // console.log('server not work');
             console.log(JSON.stringify(response));
-        }
-        
 
+        }
     });
+}
+
+function showWelcome(userInfo) {
+    // console.log("show welcome userinfo", userInfo);
+    //{id: "1", email: "aa", name: "aa", password: "aa", rights: "0", …}
+    if(userInfo) {
+        $('#loginButton').hide();
+        $('#logoutButton').show();
+        $('#loginUserName').text(userInfo.name);
+    }else {
+        $('#logoutButton').hide();
+        $('#loginButton').show();
+        $('#loginUserName').empty();
+    }
 }
 function logoutUser() {
     alert('bye');
--- dataApi/user/login.php
+++ dataApi/user/login.php
@@ -1,3 +1,53 @@
 <?
- print_r($_POST);
+session_start();
+require_once('mysql_connect.php');
+//  print_r($_POST);
+//  exit();
+// [email] => bb
+//     [password] => bb
+
+if(mysqli_connect_errno()) {
+    echo 'fail to coonect MYSQL: '.mysqli_connect_error();
+}
+
+$email = $_POST['email'];
+$password = $POST['password'];
+
+$query = "SELECT * 
+            FROM `students` 
+           WHERE `email`='$email'
+         ";
+$result = mysqli_query($conn, $query);
+    
+if($result) {
+    if(mysqli_num_rows($result) > 0) {
+        $userData = mysqli_fetch_assoc($result);
+//$userData = the column name from select above
+        // print_r($userData);
+        $output['user'] = $userData;
+        $output['success'] = true;
+        
+        $_SESSION['userID'] = $userData['id'];
+        $_SESSION['valid'] = true;            
+        // print_r($_SESSION);
+        // [userID] => 1
+        // [valid] => 1
+    } else {
+        
+        $output['error'] = 'invalide username or password';
+    }
+} else {
+    $output['error'] = 'you are not regeistered';
+}
+    
+// output:
+// "success":true,
+// "errors":[],
+// "user":{"id":"1",
+//         "email":"aa",
+//         "name":"aa",
+//         "password":"aa",
+//         "rights":"0",
+//         "token":""}
+
 ?>
